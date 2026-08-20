74bafb3af6bfc7e6106d777212f7ebb1b69dd1a8
Sujoy <sujoypaul728@gmail.com>
2026-08-21 02:18:35 +0530

    feat: enhance text page with adjustable font size and improved layout; update SideNav for dynamic link styling and add PeerShare logo

--- client/src/app/(main)/file/text/page.jsx
+++ client/src/app/(main)/file/text/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa6'
 
 export default function page() {
+
+  const[fontSize, setFontSize] = useState(20)
+
   return (
-    <div>
-        Text file page
+    <div className='bg-[#ffffff4a] backdrop-blur-xs rounded-3xl h-full p-5 border-l border-t border-[#fff]'>
+        <section className='flex justify-end pr-5 pb-2'>
+          <div className='flex items-center gap-4 bg-[#ffffff4a] backdrop-blur-xs border border-[#fff] rounded-lg px-3 py-1 select-none'>
+            <button className='flex text-xl cursor-pointer  items-center' onClick={() => setFontSize(prev => Math.min(prev + 2, 24))}>
+            a <FaAngleUp className='text-xs' />
+            </button>
+            <button className='text-xl'>{fontSize}</button>
+            <button className='flex text-md cursor-pointer  items-center' onClick={() => setFontSize(prev => Math.max(prev - 2, 12))}>
+              a <FaAngleDown className='text-xs' />
+            </button>
+          </div>
+        </section>
+        <textarea placeholder='type your text...' autoFocus name="textfile" id="textfile" className='bg-[#ffffff] text-xl text-black placeholder:text-gray-500 border border-[#fff] p-3 rounded-2xl w-full max-h-[94%] min-h-[94%] outline-none 'rows="10" column="50" style={{ fontSize: `${fontSize}px` }}></textarea>
     </div>
   )
 }
--- client/src/app/components/shared/SideNav.jsx
+++ client/src/app/components/shared/SideNav.jsx
@@ -1,17 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
-import { TiPlus } from 'react-icons/ti'
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import { TiPlus } from 'react-icons/ti';
+import Image from 'next/image';
 
 export default function SideNav() {
+  const pathname = usePathname();
+
+  const getLinkClass = (path) => {
+    return pathname === path 
+      ? 'mb-2 bg-[#ffffff8b] cursor-pointer p-3 rounded-lg border-l border-t border-[#fff]'
+      : 'mb-2 hover:bg-[#ffffff8b] cursor-pointer p-3 rounded-lg';
+  };
+
   return (
     <div className='w-75  bg-[#ffffff4a] backdrop-blur-xs rounded-3xl h-full p-5 border-l border-t border-[#fff] md:block hidden'>
         <div className='mb-8 mt-5'>
-            <h1 className='text-3xl font-bold text-orange-500'>PeerShare</h1>
+            <Link href="/"><h1 className='text-3xl font-bold text-orange-500 select-none cursor-pointer flex items-center gap-2'><Image src="/PeerShare.png" alt="PeerShare" width={40} height={40} /> PeerShare</h1></Link>
         </div>
         <ul className='text-xl'>
-            <Link href="/"><li className='mb-2 bg-[#ffffff8b] cursor-pointer p-3 rounded-lg border-l border-t border-[#fff]'>Home</li></Link>
-            <Link href="/shared"><li className='mb-2 hover:bg-[#ffffff8b] cursor-pointer p-3 rounded-lg '>Shared with me</li></Link>
-            <Link href="/join"><li className='mb-2 hover:bg-[#ffffff8b] cursor-pointer p-3 rounded-lg '>Join room</li></Link>
+            <Link href="/"><li className={getLinkClass('/')}>Home</li></Link>
+            <Link href="/shared"><li className={getLinkClass('/shared')}>Shared with me</li></Link>
+            <Link href="/join"><li className={getLinkClass('/join')}>Join room</li></Link>
         </ul>
     </div>
   )
